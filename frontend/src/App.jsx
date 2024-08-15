1559fa3d371356b4eaefb37ead5a877381b1c2a1
SADA SAIKIRAN <135319924+Saikiransada@users.noreply.github.com>
2024-08-15 15:52:29 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
+import { ThemeContext } from "./context/ThemeContext";
+
+const App = () => {
+  const { toggle } = useContext(ThemeContext);
+
+  useEffect(() => {
+    if (toggle) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [toggle]);
+  return (
+    <div className="App dark:bg-[#1b1b1f]">
+      <Router>
+        <Routes>
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
